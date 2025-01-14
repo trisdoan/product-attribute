34bf223d3647b5f3ff11efcef3aa0d3fe8e272de
trisdoan <doanminhtri8183@gmail.com>
2025-01-14 16:20:15 +0700

    [FIX] product_packaging_level: adjust rename hook to keep data

--- product_packaging_level/hooks.py
+++ product_packaging_level/hooks.py
@@ -8,9 +8,22 @@ from odoo import SUPERUSER_ID, api
 def pre_init_hook(cr):
     if openupgrade.table_exists(cr, "product_packaging_type"):
         env = api.Environment(cr, SUPERUSER_ID, {})
+        # Re-mapping the name for the xml record to be the same as the new version
+        openupgrade.logged_query(
+            cr,
+            query="""
+            UPDATE ir_model_data
+            SET name = 'product_packaging_level_default'
+            WHERE name = 'product_packaging_type_default'
+            AND module = 'product_packaging_type';
+            """,
+        )
+
         # Former version of the module is present
         models = [("product.packaging.type", "product.packaging.level")]
         openupgrade.rename_models(env.cr, models)
+        tables = [("product_packaging_type", "product_packaging_level")]
+        openupgrade.rename_tables(env.cr, tables)
         fields = [
             (
                 "product.packaging",
